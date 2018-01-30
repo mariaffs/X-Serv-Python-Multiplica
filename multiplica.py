7adb333fc2a8879ec6e9325fe41ff45ef2e09b9a
Gregorio Robles <grex@beta00.aulas.gsyc.urjc.es>
2018-01-30 12:39:50 +0100

    Ejercicio resuelto... incluso con range a pelo

---
+++ multiplica.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+for numero1 in range(1,11):
+    print("\nLa tabla del", numero1)
+    for numero2 in range(1,11):
+        print(numero1, "por", numero2, "es", numero1*numero2)
