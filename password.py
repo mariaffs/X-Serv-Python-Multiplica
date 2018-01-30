665b92772a34989ec059bf294f5d1a7a58107212
Gregorio Robles <grex@beta00.aulas.gsyc.urjc.es>
2018-01-30 12:16:40 +0100

    Ejercicio de Razwan retocado

---
+++ password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+fichero = open('/etc/passwd', 'r')
+lineas = fichero.readlines()
+diccionario = {}
+
+interesantes = ['root', 'imaginario', 'sys', 'var']
+
+for linea in lineas:
+    datos = linea[:-1].split(':')
+    usuario, shell = datos[0], datos[-1]
+    diccionario[usuario] = shell
+
+for username in interesantes:
+    try:
+        print(username, '--->', diccionario[username])
+    except KeyError:
+        print(username, "no existe")
+
+print("aaaaaaaaaaaaaaaaa")
+fichero.close()
