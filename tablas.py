d7deeffa5aaba75a05a66aa0fc2b6780a9d4a9c3
Maria Fernanda Flores Simon <mariaffs@gamma34.aulas.gsyc.urjc.es>
2019-02-05 12:48:38 +0100

    tablas

---
+++ tablas.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#Maria Fernanda Flores Simon
+
+for i in range(1,11):
+    print("Tabla del " + str(i))
+    print("-----------")
+    for j in range(1,11):
+        print(str(i) + " por " + str(j) + " es " + str(i*j))
+    
